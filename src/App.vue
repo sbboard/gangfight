7850d57165ba1adc0b4d1038039982b1284d8969
Colin Buffum <colin.buffum@gmail.com>
2018-11-10 02:42:10 -0700

    installed sass

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{windowWidth}} x {{windowHeight}}
+    <temp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Temp from './components/screen/temp.vue'
+import BottomBorder from './components/borders/bottom.vue'
+import LeftBorder from './components/borders/left.vue'
+import RightBorder from './components/borders/right.vue'
+import TopBorder from './components/borders/top.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Temp,
+    BottomBorder,
+    LeftBorder,
+    RightBorder,
+    TopBorder
+  },
+  data(){
+    return{
+      windowHeight: 0,
+      windowWidth: 0
+    }
+  },
+  created(){
+    this.windowHeight = window.innerHeight
+    this.windowWidth = window.innerWidth
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', () => {
+        this.windowHeight = window.innerHeight
+        this.windowWidth = window.innerWidth
+      });
+    })
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/screen/temp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        ddd
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="sass" scoped>
+div
+    width: 100px
+    height: 100px
+    background-color: red
+</style>
