7570a66d68d1e07c9a99bb2edd26893c2d6748cd
Colin Buffum <colin.buffum@gmail.com>
2018-11-11 15:36:00 -0700

    the business

--- src/components/screen/temp.vue
+++ src/components/screen/temp.vue
@@ -1,13 +1,37 @@
 <template>
-    <div>
-        <img src="@/assets/fortune.png"/>
-        <a href="#">BEGIN</a>
-    </div>
+<div>
+    <vgl-renderer antialias style="height: 100vh;">
+      <vgl-scene>
+        <vgl-box-geometry name="box"></vgl-box-geometry>
+        <vgl-texture src="/9volt.jpg" name="texture"></vgl-texture>
+        <vgl-mesh-standard-material name="std" map="texture"></vgl-mesh-standard-material>
+        <vgl-mesh geometry="box" material="std" :rotation="`0 0 ${y}`"></vgl-mesh>
+        <vgl-ambient-light color="#23F893"></vgl-ambient-light>
+        <vgl-directional-light position="1 1 -1"></vgl-directional-light>
+      </vgl-scene>
+    <vgl-perspective-camera orbit-position="4;"></vgl-perspective-camera>
+  </vgl-renderer>
+</div>
 </template>
 
 <script>
+
 export default {
-    
+    data(){
+        return{
+        y: 50,
+        model: "/assets/3d/Duck.gtlf"
+        }
+    },
+    methods:{
+        animate: function() {
+            requestAnimationFrame(this.animate);
+            this.y -= 0.01;
+        }
+    },
+    mounted() {
+      this.animate()
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGL from "vue-gl";
 
 Vue.config.productionTip = false
+
+Object.keys(VueGL).forEach(name => {
+    Vue.component(name, VueGL[name]);
+});
 
 new Vue({
   render: h => h(App),
