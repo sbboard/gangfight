e45704338a028de2f9d040385de67cd8a0bd2b30
sbboard <colin.buffum@gmail.com>
2019-04-23 15:43:49 -0600

    minor style to comic page

--- src/components/outerbounds/comicReader.vue
+++ src/components/outerbounds/comicReader.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="comicPage">
-        {{comicInfo.title}}
-        {{comicInfo.subtitle}}
+      <div id="theComic">
+        <h1>{{comicInfo.title}}</h1>
+        <h2>{{comicInfo.subtitle}}</h2>
         <div class="comicPages">
         <img v-for="pages in comicInfo.comicsArray" :key="pages" :src="`/assets/comics/${comicInfo.url}/${pages}`"/>
         </div>
@@ -9,6 +10,11 @@
         <div v-if="comicInfo.series !== 'noseries'">
           farLeft left {{comicInfo.series}} right farRight
         </div>
+      </div>
+      <div id="city"></div>
+      <div id="cityLights"></div>
+      <div id="cityOfStars"></div>
+      <div id="cityBlues"></div>
     </div>
 </template>
 
@@ -45,10 +51,73 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+@import "../../css/reset.css"
+@import "../../css/gangColors.sass"
+@import "../../css/gangFonts.sass"
 .navWidthMiddle
   width: 100%
 .navWidthHundred
   width: calc(100vh - 1em)
 #comicPage
   position: relative
+  min-height: 100vh
+  #theComic
+    padding: 1em 1em 6vh 1em
+    h1
+      font-size: 3em
+      text-align: center
+      color: $neonRed
+      font-family: Montserrat
+      @include textGlow($neonRed, 1px)
+    h2
+      font-size: 2em
+      text-align: center
+      margin-bottom: .5em
+      color: $neonBlue
+      font-family: Montserrat
+      @include textGlow($neonBlue, 1px)
+    .comicPages
+      img
+        max-width: 100%
+        margin: 0 auto 1em auto
+        display: block
+
+#city
+    background-image: url('/assets/global/homepage/rochester.png')
+    background-position: bottom
+    background-repeat: repeat-x
+    background-attachment: fixed
+    background-size: 100% auto
+    position: absolute
+    bottom: 0
+    opacity: .6
+    z-index: -5
+    height: 100%
+    width: 100%
+#cityOfStars
+    z-index: -7
+    position: absolute
+    width: 100%
+    height: 100%
+    top: 0px
+    left: 0px
+    background-image: url(/assets/global/homepage/starsbg.jpg)
+    margin: 0
+    padding: 0
+    background-attachment: fixed
+    background-repeat: no-repeat
+    opacity: .3
+#cityBlues
+  background: -webkit-radial-gradient(bottom, #121729, #020306 60%)
+  margin: 0
+  padding: 0
+  background-size: cover
+  background-repeat: no-repeat
+  background-position: center
+  background-attachment: fixed
+  height: 100%
+  width: 100%
+  z-index: -8
+  position: absolute
+  top: 0
 </style>
