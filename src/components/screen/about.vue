a807b1aa2245a8abfed8337e7259da1aff9c81c8
sbboard <colin.buffum@gmail.com>
2019-07-23 20:35:02 -0600

    changed email

--- src/components/screen/about.vue
+++ src/components/screen/about.vue
@@ -36,7 +36,7 @@
             <div class="windowStuff">
                 <template v-if="popUpArray[boxNumber-1] == 'email'">
                     <img id="pcImg" src="/assets/global/about/pc.png"/>
-                    <p>colin.buffum@gmail.com</p>
+                    <p>gyangufaito@gmail.com</p>
                 </template>
                 <template v-if="popUpArray[boxNumber-1] == 'cat'">
                     <img id="detailedCat" src="/assets/global/about/detailedCat.png"/>
