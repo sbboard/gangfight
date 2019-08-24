90b5e5250afeb3240d66cfeecc501355da3de1b5
sbboard <colin.buffum@gmail.com>
2019-08-23 21:37:35 -0600

    created a basic text engine for dracula

--- src/components/screen/placeholder404.vue
+++ src/components/screen/placeholder404.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div id="dayMode" v-if="hour<17||hour>5">
+        <div id="dayMode" v-if="hour<17&&hour>5">
             <div class="fourText">
                 <h1>404</h1>
                 <h2>come back when it's darker</h2>
@@ -11,14 +11,22 @@
             <div id="cityBlues"></div>
         </div>
         <div id="nightMode" v-else>
-            What's up it's me -- Dracula. Thanks for visiting my club tonight. Feel free to browse my collection of worthwhile media while you're here.
+            <div id="speechBubble" class="active">
+            <div id="faceBox"><div id="eyes">. .</div><div id="innerMouth"></div>
+            </div>
+            <div id="speechProg"></div><div id="blackBox">█</div>
+            <div id="speechText" class="hidden">What's up it's me -- Dracula. Thanks for visiting my club tonight. Feel free to browse my collection of worthwhile media while you're here.</div>
+            </div>
+            <div id="dracula">. .
+            <div id="mouth"></div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: '404',
+    name: 'DraculaCultureClub',
     data(){
         return{
             randoNum: 0,
@@ -30,6 +38,76 @@ export default {
         this.hour = today.getHours()
         //generate rando Nos for YouTube Vid
         this.randoNum = Math.floor(Math.random() * 10) + 1
+    },
+    mounted(){
+        const talk = document.getElementById("speechText").textContent;
+        let bubble = document.getElementById("speechBubble")
+        let speechProg = document.getElementById("speechProg")
+        let mouth = document.getElementById("mouth")
+        let blackBox = document.getElementById("blackBox")
+        let innerMouth = document.getElementById("innerMouth")
+        let i = 0
+        let normSpeech = 75
+
+        function speechEngine() {
+        let timeStop = normSpeech
+
+        function myLoop() {
+            setTimeout(function() {
+            speechProg.innerHTML += talk[i]
+            if (mouth.classList.contains("open")) {
+                mouth.classList.remove("open");
+                innerMouth.classList.remove("open");
+            } else {
+                mouth.classList.add("open");
+                innerMouth.classList.add("open");
+            }
+            if (talk[i] == "?" || talk[i] == "!" || talk[i] == "." || talk[i] == "-") {
+                timeStop = normSpeech * 4
+                mouth.classList.remove("open");
+                innerMouth.classList.remove("open");
+            }
+            else if(talk[i] == " "){
+                mouth.classList.remove("open");
+                innerMouth.classList.remove("open");
+                timeStop = normSpeech
+            }
+            else {
+                timeStop = normSpeech
+            }
+            i++;
+            if (i < talk.length) {
+                myLoop();
+            }
+            else{
+                mouth.classList.remove("open");
+                    bubble.classList.remove("active")
+                blackBox.classList.add("hidden");
+            }
+            }, timeStop)
+        }
+        myLoop()
+        }
+
+        function bubbleClick(){
+        if(bubble.classList.contains("active")){
+            bubble.classList.remove("active")
+            let textbox = document.getElementById("speechText")
+            i = talk.length-1
+            mouth.classList.remove("open")
+            speechProg.classList.add("hidden");
+            textbox.classList.remove("hidden");
+            bubble.classList.remove("active")
+            blackBox.classList.add("hidden");
+        }
+        else{
+            bubble.classList.add("hidden")
+        }
+        }
+
+        document.getElementById("speechBubble").addEventListener("click", bubbleClick);
+        speechEngine()
+
     }
 }
 </script>
@@ -38,6 +116,40 @@ export default {
 @import "../../css/reset.css"
 @import "../../css/gangColors.sass"
 @import "../../css/gangFonts.sass"
+
+#speechBubble
+  border: 1px solid black
+  cursor: pointer
+  #faceBox
+    float: left
+    width: 50px
+    min-height: 2em
+    height: -webkit-fill-available
+    display: block
+    #eyes
+      display: inline-block
+      width: 1em
+    #innerMouth::before
+      content: "-"
+    #innerMouth.open::before
+      content: "o"
+  .hidden
+    display: none
+  &.hidden
+    display: none
+  #speechProg
+    display: inline
+  #speechProg.hidden
+    display: none
+  #blackBox
+    display: inline
+  #blackBox.hidden
+    display: none
+#dracula
+  #mouth::before
+    content: "-"
+  #mouth.open::before
+    content: "o"
 
 #dayMode
     height: 100%
