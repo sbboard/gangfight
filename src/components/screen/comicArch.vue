7f761418f0272213b5a60889b0f7efeab7dc6dde
sbboard <colin.buffum@gmail.com>
2019-09-12 23:05:27 -0600

    moved ascii art over, moved sketch onto page

--- src/components/screen/comicArch.vue
+++ src/components/screen/comicArch.vue
@@ -1,76 +1,18 @@
 <template>
-    <div id="scene">
-        <div id="asciiPic">
-                                                                 .,,,,,..                                       
-                                                          .:f0GCt:...,..;1fC0Gfi,..                             
-                                       .,;ifLCCCCCCCCLLfLGG;.                   .:ifLCCfi.                      
-                                  ,;i1i,.                .f1t:                          .;fL1.                  
-                               ,11.                        ;;i;.                             tG1.               
-                            .1t.                            ::,;.                              .Ct              
-                         :ti,                                .i.1,                               iG:            
-                      ,t1.                                     ;ii.,                              .Li           
-                    ,L:                                          ;L..                              .fL.         
-                  .t,                                              1.i                               ;C,        
-                .t;                                                 1,1                               :C;       
-               :f,                                                   ;L.                               .C1      
-             .t,                                                      t:                                .f8:    
-           .f;                                                        iG           fGLfLLG0Gt;;,   ii    ,CLt   
-          ,t.                                                         .L1       ,ti,.       ,100Lf. fG: .CCLC:  
-         1i                                                            iL   .;1;.              ,L0ftt00t.f808C  
-       .t,                                                             .L,:f:                    18CG00CC10000; 
-      .L                                                   .           ;G;                        :80CLL0LG0GGL.
-      1.                                                ,i,  ,      ,LL;                           100CCCGfCG0C.
-     t;                                               ;i  ,i,     ,ff:                             ,G8CG00t;G00;
-    1:                                            .1Lf. :i,,   .;Cf:.                               t80G88CC000L
-   ;;                                           ,1.   1L   ..:LC;..     ...                         iG8G00C0CGGC
-  .t.                                         ,C:  :1L,.t:;CCG1..  .it1C8C00L8Cf:                   :G0f00G0G00i
-  ii                                   .:      :ff::L.ii.LCCt. . :f1;,..:tff0GG0G00t:               itGf8000C0G,
- .L.                                   f. .itt:.i;Li.CifG1C: .              ,fLLCGGG08L,            tt0G@800C8L.
- :1                                  .t,.1,:1 ,t.  ;f1GL:L.                    :G881,80001.11    . ,fC888000G0L.
- 1:                               L.;1:tt  ;iii.ii.iLLL1C;                       .:L00CLfC0fL ,    1CC088000G8t 
-.1.                              ,1:i; :fL,.1f,1i;CLCCft.                           ,tGG0Gi;.t,   :LC00800C008i 
-:1                               i.:;.i: ;tC, 1f;LCCfLf.               ...   ,:                  :Ct0ff80CG000, 
-:;                              ,i Ct:..f.  i0iLCL,ft;1     .:1G00G0G00088000t. it              .ttGCf880CCfGG  
-i:                             ,Cf1...:1fLCCti1Lf:ft,L,   .iLfttL0L::;tCG080G008C11L; ,L:       iGG1G0G800CLL1  
-1,                           .Lt.        :GG,.f;,1t,t:          :,       ,iL0008GCGC0G00fti..  .GGf0GC00GCfff.  
-f.                          :Gt.  .:;i1;   C1i1 :i ,t                        1f1CGG00CfftL:t:  L0G0GL00GGLLf:   
-L.                         ;tG,  ii    :t, ..C. Ci ;;                       ,L   ifG0:,       iGGGLfG00GLL1t    
-t,                         ,Li  11      ;i  t; ;L. t:    .:i1tLC.           1L..:L000         LG0GL000GLLft     
-L,                         ,G.  L:      ,t .L. L:  f.  .:;L80GG0C.          ;t  ;08Gf        .GC0CGG00GGL1      
-f,                         .L:  f,      ,f :1 :f  ;t    iCGCCf08G0t.         L;.:08i:        GfGG0GCG0GL1       
-;:                          LL          t1 i; 1:  f:         1;,.L8G0:       .t.  Gt        :8000G00G0Gi        
-;i                          ,8;1,  .tt:;f; f..f,  f;            :. :GCCG:     .if1L.        t8GGCG0G0C:         
-,1                           ;L, .;iC.    .L .L.  C:               ,;.  LL,i;,..         ,fLC80GCGG0G,          
-.t.                           tt..i;ti,;..f; :L.  C,    .iCi                              ;1CG0GGCCC:           
- 1.                            LC;        0, Lf.  C,  ;Gf.                                  :LC8fCC,            
- :t                              :GC:;..i,0. Cf.  C;            :;                          ..tG0G,             
-  1;                                ,tC80C0  i:   1i          :L,                            i,.,1GC:           
-   i:                                   .1CG0i:.  .         ;L.                                   CL,           
-   ,t                                         ,L888L       :1         .G:                        iC,            
-   .f,                                             .,;Cf.            :L,                        ,f.             
-    .L.                                            .1L1.            ;t                        .tti              
-      t:                                 ....,:1CGCi.                                       iCftt;              
-       ;L.                               .00i.  .L01;                                      ,CfL1f;              
-        .1G;                                :tCL.  ,LCti.                                 .f;:fi11              
-           fC.                                 .iCCt1.:GLi:                            :f1:i;ff;;i              
-            .LC,                                .1GC;;C:;L00t,                       .iC1fC:t1::;i              
-               ;GL                      1t11tC0L;,1CGL000GGtt088f.                .,f;1LifCtL,;,t:              
-                 .;fC1                   ,1CLf,,1CL0G00800000G;;tffGGft,       ,LL;f,C:tLffitCi.L:              
-                     ,tGf;                  .:ifL1:,;i;:,.fGGftt1tC11Lfit;tftLCCf11,1.t;Lt1tL;;1L,              
-                         ;fGC:,.;LL:.            .iGG,  .f0LLGfiCftti1;t;:1:t,iLL:ii:f:LtfLLi1,L;               
-                              :ifG800G000GCCG0GC1:.    :0C:LGit1,; ;C0i ,ti1:fCf;f.f;fCff1tCL.fi                
-                                                        .,1f0GCLLLLiiG.t1ii1LffL11f;G0ifiLf:.C;                 
-                                                                   :L:tifiit,CfGGGL1CGttLi;if:                  
-                                                                  iGiiCLfGGGLt;.   ;01fLf1Lt.                   
-                                                                                   GfCLGL;.                     
-                                                                                 .C1:.                          
-    </div>
+    <div id="scene" :style="fontSize">
+      <div id="sketch">
+      </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+  name: 'comicArchive',
+  computed: {
+      fontSize(){
+          return {fontSize: '1'+this.$store.getters.getTaller}
+      },
+  },
 }
 </script>
 
@@ -79,15 +21,14 @@ export default {
 @import "../../css/gangColors.sass"
 @import "../../css/gangFonts.sass"
 @import "../../css/fontawesome/css/all.css"
+
 #scene
-    background-color: black
-    #asciiPic
-        white-space: pre
-        letter-spacing: -.1em
-        font-family: "courier new"
-        font-size: .6em
-        color: $neonPink
-        position: absolute
-        left: -7em
-        top: -3.5em
+  #sketch
+    background-image: url("/assets/global/comicArch/sketch.jpg")
+    background-size: 100% 100%
+    position: absolute
+    top: 0
+    left: 0
+    width: 100em
+    height: 100em
 </style>
