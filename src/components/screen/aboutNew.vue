ceced6afba90df1f5ace1b1cc13a293cf8ccd389
sbboard <colin.buffum@gmail.com>
2021-01-01 21:33:34 -0700

    switched email back

--- src/components/screen/aboutNew.vue
+++ src/components/screen/aboutNew.vue
@@ -18,7 +18,7 @@
             <div class="windowStuff">
                 <template v-if="popUpArray[boxNumber-1] == 'email'">
                     <img id="pcImg" src="/assets/global/about/pc.png"/>
-                    <p>admin@cia.gay</p>
+                    <p>gyangu.faito@gmail.com</p>
                 </template>
                 <template v-if="popUpArray[boxNumber-1] == 'cat'">
                     <img id="detailedCat" src="/assets/global/about/detailedCat.png"/>
