4bc62954c42f5198b968cbd8dd0eda05279b2400
sbboard <colin.buffum@gmail.com>
2022-06-25 14:58:50 -0600

    fix 404 bug

--- src/router.js
+++ src/router.js
@@ -1,74 +1,104 @@
-import Vue from "vue"
-import Router from "vue-router"
-import Home from "./components/screen/Home.vue"
-import screen from "./components/screen/screen.vue"
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./components/screen/Home.vue";
+import screen from "./components/screen/screen.vue";
 
 //import Login from "./components/screen/login.vue"
-const fakeFourOFour = () => import(/* webpackChunkName: "group-mainpage" */ "./components/screen/placeholder404.vue")
-const About = () => import(/* webpackChunkName: "group-mainpage" */ "./components/screen/aboutNew.vue")
-const ComicArch = () => import(/* webpackChunkName: "group-mainpage" */ "./components/screen/comicArch.vue")
-const ProjArch = () => import(/* webpackChunkName: "group-mainpage" */ "./components/screen/projArch.vue")
+const fakeFourOFour = () =>
+  import(
+    /* webpackChunkName: "group-mainpage" */ "./components/screen/placeholder404.vue"
+  );
+const About = () =>
+  import(
+    /* webpackChunkName: "group-mainpage" */ "./components/screen/aboutNew.vue"
+  );
+const ComicArch = () =>
+  import(
+    /* webpackChunkName: "group-mainpage" */ "./components/screen/comicArch.vue"
+  );
+const ProjArch = () =>
+  import(
+    /* webpackChunkName: "group-mainpage" */ "./components/screen/projArch.vue"
+  );
 
 //pages that exist outside of borders
-const ComicReader  = () => import(/* webpackChunkName: "group-reader" */ "./components/outerbounds/comicReader.vue")
-const ArticleReader  = () => import(/* webpackChunkName: "group-reader" */ "./components/outerbounds/articleReader.vue")
+const ComicReader = () =>
+  import(
+    /* webpackChunkName: "group-reader" */ "./components/outerbounds/comicReader.vue"
+  );
+const ArticleReader = () =>
+  import(
+    /* webpackChunkName: "group-reader" */ "./components/outerbounds/articleReader.vue"
+  );
 
 //projects
-const sbboardExit = () => import("./components/projects/sbboardExit.vue")
-const picross = () => import("./components/projects/picross.vue")
+const sbboardExit = () => import("./components/projects/sbboardExit.vue");
+const picross = () => import("./components/projects/picross.vue");
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
       path: "/comicReader/:id/:cat?",
-      component: ComicReader
+      component: ComicReader,
     },
     {
       path: "/article/:id/",
-      component: ArticleReader
+      component: ArticleReader,
     },
-    { path: '/', component: screen,
-    children: [
-      {
-        path: "/",
-        name: "gang fight",
-        component: Home
-      },
-      {
-        path: "/about",
-        name: "home net",
-        component: About
-      },
-      {
-        path: "/projects",
-        name: "game shop",
-        component: ProjArch
-      },
-      {
-        path: "/comic",
-        name: "noodle cart",
-        component: ComicArch
-      },
-      {
-        path: "/sbboard",
-        name: "SBboard",
-        component: sbboardExit
-      },
-      {
-        path: "/picross",
-        name: "Picross",
-        component: picross
-      },
-      {
-        path: "/:string",
-        alias: "/*",
-        name: "Cultur Club",
-        component: fakeFourOFour
-      },
-    ]
-  },
-  ]
+    {
+      path: "/",
+      component: screen,
+      children: [
+        {
+          path: "/",
+          name: "gang fight",
+          component: Home,
+        },
+        {
+          path: "/about",
+          name: "home net",
+          component: About,
+        },
+        {
+          path: "/projects",
+          name: "game shop",
+          component: ProjArch,
+        },
+        {
+          path: "/comic",
+          name: "noodle cart",
+          component: ComicArch,
+        },
+        {
+          path: "/sbboard",
+          name: "SBboard",
+          component: sbboardExit,
+        },
+        {
+          path: "/picross",
+          name: "Picross",
+          component: picross,
+        },
+        {
+          path: "/:string",
+          name: "Cultur Club",
+          component: fakeFourOFour,
+        },
+      ],
+    },
+    {
+      path: "/*",
+      component: screen,
+      children: [
+        {
+          path: "/",
+          name: "Cultur Club",
+          component: fakeFourOFour,
+        },
+      ],
+    },
+  ],
 });
