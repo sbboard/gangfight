4c2c80d5e098ad9f43fcd30738b488450253e58c
sbboard <colin.buffum@gmail.com>
2022-11-07 20:21:11 -0700

    remove lazy loading

--- src/main.js
+++ src/main.js
@@ -1,22 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueLazyload from 'vue-lazyload'
-import VueMeta from 'vue-meta'
+import Vue from "vue";
+import App from "./App.vue";
+import VueMeta from "vue-meta";
 
-import router from './router.js';
-import {store} from './store.js';
+import router from "./router.js";
+import { store } from "./store.js";
 
-Vue.config.productionTip = false
-Vue.use(VueLazyload, {
-  preLoad: 1.3,
-  //loading: '/assets/global/comics/nerv2.gif',
-  attempt: 3,
-})
+Vue.config.productionTip = false;
 
-Vue.use(VueMeta)
+Vue.use(VueMeta);
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
